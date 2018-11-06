0d8e68a6566f05b626865e45f8d5382e475eb667
nikhilagrawal05 <nikhil.agrawal@sjsu.edu>
2018-11-05 21:41:50 -0800

    Adding fuctions

--- Recommendations.py
+++ Recommendations.py
@@ -39,4 +39,27 @@ class Recommendation_System:
         testset = data_train.build_anti_testset()
         return testset
 
- 
+    def store_predictions(self, testset,algo):
+        result = []
+        for row in testset:
+            result.append(algo.predict(row[0], row[1]))
+        return result
+
+    def get_recommendations(self, predictions, topN):
+        
+        for uid, iid, r_ui, est, details in predictions:
+            self.top_recs[uid].append((iid, est))
+
+        for uid, user_ratings in self.top_recs.items():
+            user_ratings.sort(key=lambda x: x[1], reverse=True)
+            self.top_recs[uid] = user_ratings[:topN]
+
+        return self.top_recs
+
+    def get_recommendation_for_uid(self, user_id):
+        for uid, user_ratings in self.top_recs.items():
+            if (uid == user_id):
+                return uid, ([iid for (iid, _) in user_ratings])
+        return ''
+
+
